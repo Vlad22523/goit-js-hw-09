16dfda007ee9388f9a9878e2c7e46d8404bbc2ef
Vlad Melnyk <vlad22523@gmail.com>
2024-05-14 05:12:35 +0300

    goit-js-hw-09

---
+++ src/js/2-form.js
@@ -0,0 +1,38 @@
+let formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const formDataKey = 'feedback-form-state';
+let savedFormData = JSON.parse(localStorage.getItem(formDataKey));
+
+form.addEventListener('input', saveData);
+form.addEventListener('submit', sendData);
+
+if (savedFormData) {
+  formData = savedFormData;
+}
+
+function saveData(event) {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
+  localStorage.setItem(formDataKey, JSON.stringify(formData));
+}
+
+form.email.value = formData.email;
+form.message.value = formData.message;
+
+function sendData(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  localStorage.removeItem(formDataKey);
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+}
